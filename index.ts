d07de815e62084b6ef247460c5f4e70914381000
ClydeFrog04 <egan.randal@gmail.com>
2020-09-02 18:55:59 -0600

    chore(index.ts): session setup
    Setup express session and connect-session-knex in index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 //imports
 import express from "express";
 import {usersRouter} from "./routes/users/usersRoute";
+import session from "express-session";
+import {db} from "./data/dbcofig";
+const KnexSessionStore = require("connect-session-knex")(session);
 
 //objects
 const server = express();
@@ -8,6 +11,15 @@ const port = process.env.PORT || 4000;
 
 //server uses
 server.use(express.json());
+server.use(session({
+    resave: false, // avoid recreating sessions that have not changed
+    saveUninitialized: false, // to comply with GDPR laws
+    secret: "keep it secret, keep it safe", // cryptographically sign the cookie
+    store: new KnexSessionStore({
+        knex: db, // configured instance of knex
+        createtable: true, // if the sessions table doesn't exist, create it automatically
+    }),
+}))
 server.use("/api", usersRouter);
 
 server.listen(port, () => {
